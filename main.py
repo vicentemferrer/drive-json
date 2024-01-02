8222ff6892a797a566747dd3ed10b560dcce3f3a
Vicente <vicenteferrer_01@hotmail.com>
2024-01-02 15:47:48 -0300

    write_json and format_data added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
+import json
 
 credentials_auth = "credentials_module.json"
 
@@ -18,7 +19,7 @@ def login():
     
     return GoogleDrive(gauth)
 
-def list_content(query):
+def list_content(query: str):
     results = []
     gdrive = login()
 
@@ -29,16 +30,30 @@ def list_content(query):
     
     return results
 
-def main():
+def format_data(results_list: list):
     url_template = "https://drive.google.com/uc?export=download&id="
 
+    def add_url(file_dict: dict):
+        file_list = list(file_dict.items())
+
+        file_list.append(['url', url_template + file_dict['id']])
+
+        return { prop[0]: prop[1] for prop in file_list }
     
+    return list(map(add_url, results_list))
+
+def write_json(data):
+    data_to_dump = { 'files': data }
+    
+    with open('drive-files.json', 'w') as file:
+        json.dump(data_to_dump, file, indent=4)
+
+def main():
     query = f"'{folder_id}' in parents and trashed=false"
 
-    results = list_content(query)
+    results = format_data(list_content(query))
 
-    print(results)
-
+    write_json(results)
 
 if __name__ == "__main__":
     main()
